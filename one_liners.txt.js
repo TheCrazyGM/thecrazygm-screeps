f468931379bf7e9f5c6ec546737f42e415143aeb
Michael Garcia <thecrazygm@gmail.com>
2020-03-02 12:02:16 -0600

    initial

---
+++ one_liners.txt.js
@@ -0,0 +1,5 @@
+Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], 'HarvesterBig', { memory: { role: 'harvester' } } );
+
+Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], "Upgrader-2", {memory: {role: 'upgrader'}});
+
+Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], "Builder-2", {memory: {role: 'builder'}});
