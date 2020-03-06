6a4e88a70df7ed7e0ec69f31fd223c28597f1745
Michael Garcia <thecrazygm@gmail.com>
2020-03-06 17:16:40 -0600

    min creeps

--- default/prototype.spawn.js
+++ default/prototype.spawn.js
@@ -72,8 +72,7 @@ StructureSpawn.prototype.spawnCreepsIfNecessary =
                     }
                 }
                 // if no claim order was found, check other roles
-                else if (this.memory.hasOwnProperty(this.memory.minCreeps) && this.memory.hasOwnProperty(this.memory.minCreeps[role])
-                    && numberOfCreeps[role] < this.memory.minCreeps[role]) {
+                else if (numberOfCreeps[role] < this.memory.minCreeps[role]) {
                     if (role == 'lorry') {
                         name = this.createLorry(150);
                     }
