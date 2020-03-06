c4216a6d822931b99fab5988d2fe447d0cc4bc53
Michael Garcia <thecrazygm@gmail.com>
2020-03-05 22:29:06 -0600

    misprint (console.log)

--- default/main.js
+++ default/main.js
@@ -23,9 +23,10 @@ module.exports.loop = function () {
         });
         console.log(Game.rooms[name].controller.progress, Game.rooms[name].controller.progressTotal, percent);
     }
+    /*
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
-    /*
+    
     if (harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
