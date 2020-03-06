35f1cf430a73125a1cc1db26f1081ffd46ec02ab
Michael Garcia <thecrazygm@gmail.com>
2020-03-06 02:20:40 -0600

    tower logic

--- default/prototype.tower.js
+++ default/prototype.tower.js
@@ -3,6 +3,9 @@ StructureTower.prototype.defend =
     function () {
         // find closes hostile creep
         var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
         // if one is found...
         if (target != undefined) {
             // ...FIRE!
