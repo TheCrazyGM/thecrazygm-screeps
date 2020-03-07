627040bf370b6cc4fb0f1e04a52cef066da1e839
Michael Garcia <thecrazygm@gmail.com>
2020-03-07 14:49:02 -0600

    clearlog()

--- default/main.js
+++ default/main.js
@@ -54,4 +54,7 @@ module.exports.loop = function () {
         // run spawn logic
         Game.spawns[spawnName].spawnCreepsIfNecessary();
     }
+    function clearLog(){
+        console.log("<script>angular.element(document.getElementsByClassName('fa fa-trash ng-scope')[0].parentNode).scope().Console.clear()</script>")
+    }
 }
