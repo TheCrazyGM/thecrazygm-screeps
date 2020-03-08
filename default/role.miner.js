bbc20bff5c44657e09f8c8b52ad2c252101ae4b4
Michael Garcia <thecrazygm@gmail.com>
2020-03-07 18:59:39 -0600

    fixing miner

--- default/role.miner.js
+++ default/role.miner.js
@@ -5,7 +5,7 @@ module.exports = {
         let source = Game.getObjectById(creep.memory.sourceId);
         console.log(source)
         // find container next to source
-        let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+        let container = this.room.find(FIND_STRUCTURES, 1, {
             filter: s => s.structureType == STRUCTURE_CONTAINER
         })[0];
 
