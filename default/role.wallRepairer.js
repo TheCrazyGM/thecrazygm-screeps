57a7869372fdc776138f81df3ee7780697f5918d
Michael Garcia <thecrazygm@gmail.com>
2020-03-08 00:55:29 -0600

    don't get from container

--- default/role.wallRepairer.js
+++ default/role.wallRepairer.js
@@ -59,7 +59,7 @@ module.exports = {
         }
         // if creep is supposed to get energy
         else {
-            creep.getEnergy(true, true);
+            creep.getEnergy(false, true);
         }
     }
 };
