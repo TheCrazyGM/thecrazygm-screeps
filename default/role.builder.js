7956662b8f20a24b4c5c14b8f362f03fbdebe375
Michael Garcia <thecrazygm@gmail.com>
2020-03-08 15:27:27 -0500

    no containers

--- default/role.builder.js
+++ default/role.builder.js
@@ -47,7 +47,7 @@ module.exports = {
         }
         // if creep is supposed to get energy
         else {
-            creep.getEnergy(true, true);
+            creep.getEnergy(false, true);
         }
     }
 };
