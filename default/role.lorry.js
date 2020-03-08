c5afe1bdc60771f8a5c62d3d012925a40100d47f
Michael Garcia <thecrazygm@gmail.com>
2020-03-08 06:55:11 -0500

    change speech

--- default/role.lorry.js
+++ default/role.lorry.js
@@ -6,7 +6,7 @@ module.exports = {
         if (creep.memory.working == true && creep.carry.energy == 0) {
             // switch state
             creep.memory.working = false;
-            creep.say('🔄 retrieve');
+            creep.say('🔄 pick up');
         }
         // if creep is harvesting energy but is full
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
