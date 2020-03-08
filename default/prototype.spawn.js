d50fa32e8a2469b615db4cc9d5ad54689940c08a
Michael Garcia <thecrazygm@gmail.com>
2020-03-08 14:37:17 -0500

    koolaid

--- default/prototype.spawn.js
+++ default/prototype.spawn.js
@@ -23,7 +23,7 @@ StructureSpawn.prototype.koolaid =
         /** @type {Array.<Creep>} */
         let creepsInRoom = room.find(FIND_MY_CREEPS);
         for (var name of creepsInRoom) {
-            room.creeps[name].suicide()
+            name.suicide()
         }
     }
 
